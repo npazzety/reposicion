782c5f2f5eaab754fea5ae54df88a212ec88afa3
npazzety <nelsonpazzety10@gmail.com>
2024-04-29 11:12:34 -0600

    reposicion

---
+++ src/main/java/hn/lenguajes/reposicion/ReposicionApplication.java
@@ -0,0 +1,13 @@
+package hn.lenguajes.reposicion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReposicionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReposicionApplication.class, args);
+	}
+
+}
